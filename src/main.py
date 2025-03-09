fbb5fbe73b29d47226369600677c5fb7abb0cef9
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2025-03-09 08:55:31 +0000

    first working backend

--- src/main.py
+++ src/main.py
@@ -1,17 +1,46 @@
-from fastapi import FastAPI 
-from fastapi.middleware.cors import CORSMiddleware 
-from .model import pipe 
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
+from PIL import Image
+import io
+
+from .model import pipe
+
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def home():
-    return {"messange":"hello world"}
+    return {"message": "hello world"}
 
+@app.post("/predict/")
+async def create_file(file: UploadFile = File(...)):
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="Sorry, you need to upload an image.")
 
-@app.post('/predict')
+    try:
+        contents = await file.read()
 
-async def predict_image():
-    return {...}
-    
+        image = Image.open(io.BytesIO(contents))
+        image.verify()  
+
+        image = Image.open(io.BytesIO(contents))
+
+        prediction = pipe(image)
+
+        return {
+            "filename": file.filename,
+            "content_type": file.content_type,
+            "prediction": prediction
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Sorry, you need to upload a valid image.")
+
